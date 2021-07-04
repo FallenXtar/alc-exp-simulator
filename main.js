4e98811817af59984751deffd22da8b9946c8711
XTARZ <i@xtarz.cn>
2021-07-04 14:59:00 +0800

    重构代码，使用一维地图算法

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 /* 模型基本参数设置 */
 
 //玩家数量
-var playerAmount = 100;
+var playerAmount = 50000;
 
 // 随机战斗设定运行次数
-var runTimes = 1000;
+var runTimes = 10000;
 
 /* 初始化环境 */
-var playerList = [];
-var timesRun = 0;
-var modelRun = 0;
+function initModel() {
+  playerList = [];
+  timesRun = 0;
+  modelRun = 0;
+}
 
 function genPlayer(amount) {
   playerList = [];
@@ -57,25 +59,43 @@ function calcExp(amount) {
 // 运行模型的函数
 function runModel(amount) {
   for (let index = 0; index < amount; index++) {
+    initModel();
     genPlayer(playerAmount);
     for (let index = 0; index < runTimes; index++) {
       selectBattle();
     }
+
     modelRun++;
 
     // console.log("Times set to run: " + runTimes);
     // console.log("Times actually run: " + timesRun);
     // console.log("Highest Exp is: " + _.max(playerList));
     // console.log("Total Exp is: " + _.sum(playerList));
+    document.getElementById("div1").appendChild("p") =
+      "Model now on " +
+      modelRun +
+      "<br/>" +
+      "Times actually run: " +
+      timesRun +
+      "<br/>" +
+      "Highest Exp is: " +
+      _.max(playerList) +
+      "<br/>" +
+      "Total Exp is: " +
+      _.sum(playerList) +
+      "<br/>" +
+      "<br/>";
 
-    document.write("Model now on " + modelRun + "<br/>");
-    document.write("Times set to run: " + runTimes + "<br/>");
-    document.write("Times actually run: " + timesRun + "<br/>");
-    document.write("Highest Exp is: " + _.max(playerList) + "<br/>");
-    document.write("Total Exp is: " + _.sum(playerList) + "<br/>");
-    document.write("<br/>");
+    // document.write("Model now on " + modelRun + "<br/>");
+    // document.write("Times actually run: " + timesRun + "<br/>");
+    // document.write("Highest Exp is: " + _.max(playerList) + "<br/>");
+    // document.write("Total Exp is: " + _.sum(playerList) + "<br/>");
+    // document.write("<br/>");
   }
 }
 
 /* 展示结果 */
-runModel(5);
+
+
+
+
