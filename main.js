6d9976d304d900d01922a8bbeb016d770f671087
XTARZ <i@xtarz.cn>
2021-07-05 21:52:30 +0800

    优化坐标算法

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* 沙盒基本参数设置 */
 
-var sandboxCapicity = 1000;
+var sandboxCapicity = 10;
 // 定义最大玩家数量；
 
-var sandboxMapSize = 1000;
+var sandboxMapSize = 10;
 // 定义一维地图大小，地图会向原点右边扩展这个大小；
 
 /* 生成沙盒对象 */
@@ -60,41 +60,48 @@ var Player = {
   location: NaN,
 
   set setLocation(target) {
+    _.pull(Sandbox.map[this.location], this.id);
     this.location = target;
-    Sandbox.map[target].push(this);
+    Sandbox.map[target].push(this.id);
   },
 
   get kd() {
     return this.kill + "/" + this.death;
   },
-  // 返回玩家的击杀/死亡比
+  // 实用属性，返回玩家的击杀/死亡比
 
   get timesBattled() {
     return this.kill + this.death;
   },
-  // 返回玩家的总战斗场次
+  // 实用属性，返回玩家的总战斗场次
 
   get level() {
     return expToLevel(this.status);
   },
 
+  // 实用属性，返回玩家的当前等级，和多余的经验数量，以数组呈现
+
   win(exp) {
     this.status = this.status + expTransfer(exp);
     this.kill++;
   },
+  // 玩家战斗胜利，传入参数为对方的经验值
 
   dead() {
     this.status = -1; // 状态记为死亡
     this.death++; // 记录死亡次数
-    this.location =NaN;
+    this.location = NaN;
+    _.pull(Sandbox.map[this.location], this.id);
   },
+  // 玩家死亡，经验值变为-1，从地图上消失。
 
   battleLog: [],
 
   //玩家的战斗记录
 
   move(path) {
-    this.location = fixLocation(this.location + path);
+    this.setLocation = fixLocation(this.location + path);
+    console.log(Sandbox.map);
   },
 };
 
@@ -103,7 +110,7 @@ var Player = {
 function expToLevel(exp) {
   let level = 0;
   let index = 1;
-  while (index < exp) {
+  while (index <= exp) {
     level++;
     exp -= index;
     index++;
@@ -135,27 +142,26 @@ function battle(params) {
       params.sort(function () {
         return 0.5 - Math.random();
       });
-      while (params.length > 1) {
-        params.splice(fight(params[0], params[1]), 1);
-      }
+      while (params.length > 1) {}
       // 这里放打架时候的代码
       break;
   }
 }
 
 function fight(a, b) {
-  let ra = a.status + 1;
-  let rb = b.status + 1;
-  if (_.random(ra + rb - 1) < ra) {
-    a.win(b.status);
+  // 仅传入战斗双方的ID
+  let ra = _.find(Sandbox.playerList, { id: a });
+  let rb = _.find(Sandbox.playerList, { id: b });
+  if (_.random(ra.status + rb.status + 1) < ra.status + 1) {
+    ra.win(rb.status);
 
-    b.dead();
+    rb.dead();
 
     return 0;
   } else {
-    b.win(a.status);
+    rb.win(ra.status);
 
-    a.dead();
+    ra.dead();
 
     return 1;
   }
@@ -184,3 +190,8 @@ function fixLocation(a) {
   }
 }
 // 这个函数将坐标限制在MapSize范围内，即使得这个坐标系实际上是一个一维封闭坐标系，可以考虑每次坐标处理都套一层这个函数
+
+Sandbox.initMap();
+Sandbox.initPlayer();
+console.log(Sandbox.playerList);
+console.log(Sandbox.map);
