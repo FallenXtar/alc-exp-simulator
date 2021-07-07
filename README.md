b20efdee7fbb2acbe2bc6bcb00fe86206d333b1b
XTARZ <i@xtarz.cn>
2021-07-07 10:57:26 +0800

    精简了前端代码，增加了背景元素、完善了页脚。

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # 众生之门游戏世界经验模拟
 
 #### 介绍
+
 众生之门游戏世界经验模拟
 
 #### 软件架构
-软件架构说明
 
+采用纯 Javascript 浏览器后端，前端使用 Vue + Element Plus 搭建。
 
 #### 安装教程
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1. 克隆仓库
+2. 在根目录启动网页服务器
+3. 访问 `index.html`
 
 #### 使用说明
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1.  在参数设置中填入模型的初始参数
+2.  点击 `更新参数`
+3.  点击 `初始化模型`
+4.  运行模型
 
 #### 参与贡献
 
@@ -25,13 +27,3 @@
 2.  新建 Feat_xxx 分支
 3.  提交代码
 4.  新建 Pull Request
-
-
-#### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 const MainWindow = {
   data() {
     return {
-      maxPlayer: sandboxCapicity,
-      mapSize: sandboxMapSize,
       tableData: Sandbox.playerList,
       settings: {
-        maxPlayer: 10,
+        maxPlayer: sandboxCapicity,
+        mapSize: sandboxMapSize,
       },
     };
   },
+
+  created() {
+    this.$alert(
+      "请勿尝试使用异常或过大的参数初始化模型！可能导致你的浏览器冻结甚至设备死机！",
+      "重要提示",
+      {
+        confirmButtonText: "知道了",
+      }
+    );
+  },
   methods: {
     update() {
-      sandboxCapicity = +this.maxPlayer;
-      sandboxMapSize = +this.mapSize;
+      sandboxCapicity = +this.settings.maxPlayer;
+      sandboxMapSize = +this.settings.mapSize;
+      this.$message.success({ message: "参数已更新", center: true });
     },
     init() {
-      initModel();
-      this.tableData = Sandbox.playerList;
+      try {
+        initModel();
+        this.tableData = Sandbox.playerList;
+      } catch (error) {
+        this.$notify.error({
+          title: "有错误发生",
+          message: "查看控制台获取详情",
+          duration: 0,
+        });
+        throw error;
+      }
+    },
+    start() {
+      this.$message({ message: "什么都没有发生，因为还没写好", center: true });
     },
   },
 };
